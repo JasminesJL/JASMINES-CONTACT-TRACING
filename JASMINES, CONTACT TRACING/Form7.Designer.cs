e2a1fc5e22981a18d4530600dca18f3f67ab71cd
John Lloyd A. Jasmines <jasminesj37@gmail.com>
2022-07-03 14:58:29 +0800

    Button Scan qr code is now working

--- JASMINES, CONTACT TRACING/Form7.Designer.cs
+++ JASMINES, CONTACT TRACING/Form7.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F7));
             this.SuspendLayout();
             // 
             // F7
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "F7";
             this.Text = "QR CODE SCANNER";
             this.ResumeLayout(false);
